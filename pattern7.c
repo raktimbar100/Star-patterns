eb720427701f653889ea44bbe96b729d2a4b470a
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-07-04 03:01:09 +0530

    Create pattern7.c

---
+++ pattern7.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	printf("Enter the numbers:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			printf(" ");
+		}
+		for(j=0;j<=i;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+       
+
+}
